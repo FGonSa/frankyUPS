d3bf12a8949529d19eeb46e2c617fdcbe7af1cc5
Frank Santos <fransecre@gmail.com>
2023-05-13 18:12:11 +0200

    Stepzen init

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,16 +1,48 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import CustomerScreen from '../screens/CustomerScreen'
 import OrdersScreen from '../screens/OrdersScreen'
+import { useNavigation } from '@react-navigation/native'
+import { Icon } from '@rneui/themed'
 
-export type TabStackParamList = {}
 
-const Tab = createBottomTabNavigator()
+//Sólo podemos aceptar estos valores
+export type TabStackParamList = {
+    Customers: undefined;
+    Orders: undefined;
+}
+
+const Tab = createBottomTabNavigator<TabStackParamList>()
 
 const TabNavigator = () => {
+    const navigation = useNavigation()
+
+    //Se dispara al renderizar el componente
+    //Oculta el header
+    //Al no haber dependencias se dispara sólo la primera vez que se renderiza
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={({route}) => ({
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "grey",
+        tabBarIcon: ({focused, color, size}) => {
+            if (route.name === 'Customers'){
+                return (
+                    <Icon name="users" type="entypo" color={focused ? "blue" : "gray"} />
+                )
+            }else if(route.name === "Orders"){
+                return (
+                    <Icon name="box" type="entypo" color={focused ? "red" : "gray"} />
+                )
+            }
+        }
+    })}>
       <Tab.Screen name="Customers" component={CustomerScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
     </Tab.Navigator>
