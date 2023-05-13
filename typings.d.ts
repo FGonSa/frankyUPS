24c002879d741409d24cd2863d135e216b589f9b
Frank Santos <fransecre@gmail.com>
2023-05-13 22:29:59 +0200

    useOrder custom Hook

--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,32 @@ type CustomerList = {
     name: IDBCursor;
     value: Customer;
 }
+
+type TrackingItems = {
+    customer_id: ID;
+    customer: Customer;
+    items: Item[];
+}
+
+type Item = {
+    item_id: ID;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+type Order = {
+    carrier: string;
+    createdAt: string;
+    shippingCost: number;
+    trackingId: string;
+    trackingItems: TrackingItems;
+    Lat: number;
+    Lng: number;
+    Address: string;
+    City: string;
+}
+
+type OrderResponse = {
+    value: Order;
+}
