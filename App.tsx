47208f8212fd2df55ca06e470d3d7a6779c10e5f
Frank Santos <fransecre@gmail.com>
2023-05-13 20:56:26 +0200

    Apollo Client

--- App.tsx
+++ App.tsx
@@ -4,14 +4,22 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://dashboard.stepzen.com/explorer?endpoint=api%2Fhusky-data',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider
     <TailwindProvider utilities={utilities}>
+      <ApolloProvider client={client}>
       <NavigationContainer>
         <RootNavigator />
      </NavigationContainer>
+     </ApolloProvider>
    </TailwindProvider>
     
   );
