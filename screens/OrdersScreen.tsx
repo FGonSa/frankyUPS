d22a0dfa4589fc2ff5acd3508c6db766faddd256
Frank Santos <fransecre@gmail.com>
2023-05-14 15:28:53 +0200

    OrdersScreen

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { TabStackParamList } from '../navigator/TabNavigator'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamsList } from '../navigator/RootNavigator'
+import { useTailwind } from 'tailwind-rn/dist'
+import { Image } from '@rneui/themed'
 
+export type OrdersScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList, "Orders">, NativeStackNavigationProp<RootStackParamsList>>
 const OrdersScreen = () => {
+  const tw= useTailwind()
+  const navigation = useNavigation<OrdersScreenNavigationProp>()
+  const [ascending, setAscending] = useState<boolean>(false)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({focused, color})=> (
+        <Text style={{ color: focused ? "#EB6A7C" : color, fontSize: 10}}>Orders</Text>
+      )
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>OrdersScreen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{  }}/>
+    </ScrollView>
   )
 }
 
