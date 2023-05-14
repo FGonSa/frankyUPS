0458312d3eefe697df5ccb4e3440dda039a3b96a
Frank Santos <fransecre@gmail.com>
2023-05-14 21:37:28 +0200

    Demo

--- components/DeliveryCard.tsx
+++ components/DeliveryCard.tsx
@@ -2,17 +2,18 @@ import { View, Text } from "react-native";
 import React from "react";
 import { useTailwind } from "tailwind-rn/dist";
 import { Card, Divider, Icon } from "@rneui/themed";
-import MapView, {Marker} from "react-native-maps"
-type Props = { order: Order };
+import MapView, { Marker } from "react-native-maps";
 
-const DeliveryCard = ({ order }: Props) => {
+type Props = { order: Order; fullWidth?: boolean };
+
+const DeliveryCard = ({ order, fullWidth }: Props) => {
   const tw = useTailwind();
   return (
     <Card
       containerStyle={[
-        tw("rounded-lg my-2"),
+        tw(`${fullWidth ? "rounded-none m-0" : "rounded-lg"} my-2`),
         {
-          backgroundColor: "#59C1CC",
+          backgroundColor: fullWidth ? "#EB6A7C" : "#59C1CC",
           padding: 0,
           paddingTop: 16,
           shadowColor: "black",
@@ -22,38 +23,41 @@ const DeliveryCard = ({ order }: Props) => {
         },
       ]}
     >
-      <View>
+      <View style={fullWidth && {height: "100%"}}>
         <Icon name="box" type="entypo" size={50} color="white" />
 
         <View>
-          <Text style={tw("text-xs text-center uppercase text-white font-bold")}>
+          <View style={tw("mx-auto")}>
+          <Text
+            style={tw("text-xs text-center uppercase text-white font-bold")}
+          >
             {order.carrier} - {order.trackingId}
           </Text>
-          <Text>
+          <Text style={tw("text-white text-center text-lg font-bold")}>
             Expected Delivery: {new Date(order.createdAt).toLocaleDateString()}
           </Text>
           <Divider color="white" />
         </View>
 
         <View style={tw("mx-auto pb-10")}>
-        <Text style={tw("text-base text-center text-white font-bold mt-5")}>
-          Address
-        </Text>
+          <Text style={tw("text-base text-center text-white font-bold mt-5")}>
+            Address
+          </Text>
 
-        <Text style={tw("text-sm text-center text-white")}>
-          {order.Address}, {order.City}
-        </Text>
+          <Text style={tw("text-sm text-center text-white")}>
+            {order.Address}, {order.City}
+          </Text>
 
-        <Text style={tw("text-sm text-center italic text-white")}>
-          Shipping Cost: {order.shippingCost}€
-        </Text>
+          <Text style={tw("text-sm text-center italic text-white")}>
+            Shipping Cost: {order.shippingCost}€
+          </Text>
         </View>
       </View>
       <Divider color="white" />
 
       <View style={tw("p-5")}>
         {order.trackingItems.items.map((item) => (
-          <View style={tw("flex-row justify-between items-center")}>
+          <View key={item.item_id} style={tw("flex-row justify-between items-center")}>
             <Text style={tw("text-sm italic text-white")}>{item.name}</Text>
             <Text style={tw("text-white text-xl")}>{item.quantity}</Text>
           </View>
@@ -61,26 +65,27 @@ const DeliveryCard = ({ order }: Props) => {
       </View>
 
       <MapView
-      initialRegion={{ 
-        latitude: order.Lat,
-        longitude: order.Lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005
-       }}
-       style={[tw("w-full"), {height: 200}]}
+        initialRegion={{
+          latitude: order.Lat,
+          longitude: order.Lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        style={[tw("w-full"), {flexGrow: 1}, !fullWidth && { height: 200 }]}
       >
         {order.Lat && order.Lng && (
-          <Marker 
-          coordinate={{ 
-            latitude: order.Lat,
-            longitude: order.Lng,
-           }}
-           title="Delivery Location"
-           description={order.Address}
-           identifier="destination"
+          <Marker
+            coordinate={{
+              latitude: order.Lat,
+              longitude: order.Lng,
+            }}
+            title="Delivery Location"
+            description={order.Address}
+            identifier="destination"
           />
         )}
       </MapView>
+      </View>
     </Card>
   );
 };
--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -1,15 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TabNavigator from './TabNavigator';
-import ModalScreen from '../screens/ModalScreen';
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./TabNavigator";
+import ModalScreen from "../screens/ModalScreen";
+import OrderScreen from "../screens/OrderScreen";
 
 //Lista de Types que aceptara el RootStack en sus params
 export type RootStackParamsList = {
-    Main: undefined;
-    MyModal: {userId: string; name: String},
-    Order: {order: any}
-}
+  Main: undefined;
+  MyModal: { userId: string; name: String };
+  Order: { order: Order };
+};
 
 const RootStack = createNativeStackNavigator<RootStackParamsList>();
 
@@ -21,10 +22,21 @@ const RootNavigator = () => {
       </RootStack.Group>
 
       <RootStack.Group screenOptions={{ presentation: "modal" }}>
-        <RootStack.Screen options={{headerShown: false}} name="MyModal" component={ModalScreen} />
+        <RootStack.Screen
+          options={{ headerShown: false }}
+          name="MyModal"
+          component={ModalScreen}
+        />
+      </RootStack.Group>
+
+      <RootStack.Group>
+        <RootStack.Screen
+          name="Order"
+          component={OrderScreen}
+        />
       </RootStack.Group>
     </RootStack.Navigator>
-  )
-}
+  );
+};
 
-export default RootNavigator
+export default RootNavigator;
