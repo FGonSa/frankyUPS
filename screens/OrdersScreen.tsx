2bd19d8c573e5e05600b4200da2fc713942f5587
Frank Santos <fransecre@gmail.com>
2023-05-14 15:59:33 +0200

    Orders Mapping

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,33 +1,70 @@
-import { View, Text, ScrollView } from 'react-native'
-import React, { useLayoutEffect, useState } from 'react'
-import { CompositeNavigationProp, useNavigation } from '@react-navigation/native'
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
-import { TabStackParamList } from '../navigator/TabNavigator'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { RootStackParamsList } from '../navigator/RootNavigator'
-import { useTailwind } from 'tailwind-rn/dist'
-import { Image } from '@rneui/themed'
+import { View, Text, ScrollView, ActivityIndicator } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamsList } from "../navigator/RootNavigator";
+import { useTailwind } from "tailwind-rn/dist";
+import { Button, Image } from "@rneui/themed";
+import OrderCard from "../components/OrderCard";
+import useOrders from "../hooks/useOrders";
 
-export type OrdersScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<TabStackParamList, "Orders">, NativeStackNavigationProp<RootStackParamsList>>
+export type OrdersScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
+  NativeStackNavigationProp<RootStackParamsList>
+>;
 const OrdersScreen = () => {
-  const tw= useTailwind()
-  const navigation = useNavigation<OrdersScreenNavigationProp>()
-  const [ascending, setAscending] = useState<boolean>(false)
-
+  const tw = useTailwind();
+  const navigation = useNavigation<OrdersScreenNavigationProp>();
+  const [ascending, setAscending] = useState<boolean>(false);
+  const { loading, error, orders } = useOrders();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
-      tabBarLabel: ({focused, color})=> (
-        <Text style={{ color: focused ? "#EB6A7C" : color, fontSize: 10}}>Orders</Text>
-      )
+      tabBarLabel: ({ focused, color }) => (
+        <Text style={{ color: focused ? "#EB6A7C" : color, fontSize: 10 }}>
+          Orders
+        </Text>
+      ),
     });
   }, []);
 
   return (
-    <ScrollView>
-      <Image source={{  }}/>
-    </ScrollView>
-  )
-}
+    <ScrollView style={{ backgroundColor: "#EB6A7C" }}>
+      <Image
+        PlaceholderContent={<ActivityIndicator />}
+        containerStyle={tw("w-full h-64")}
+        source={{ uri: "https://links.papareact.com/m51" }}
+      />
 
-export default OrdersScreen
+      <View>
+        <Button
+          color="pink"
+          titleStyle={{ color: "grey", fontWeight: "400" }}
+          style={tw("py-2 px-5")}
+          onPress={() => setAscending(!ascending)}
+        >
+          {ascending ? "Showing: Oldest First" : "Showing: Most Recent First"}
+        </Button>
+
+        {orders
+          ?.sort((a, b) => {
+            if (ascending) {
+              return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+            } else {
+              return new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1;
+            }
+          })
+          .map((order) => (
+            <OrderCard key={order.trackingId} item={order} />
+          ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default OrdersScreen;
